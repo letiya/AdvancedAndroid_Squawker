77a3cc94b64ef79b46e4ca65951de68ca7cc7898
Belle Lee <letiya66@gmail.com>
2018-07-03 23:17:20 -0700

    3. Advanced Android App Development - Lesson 4 Firebase Cloud Messaging - 12. Exercise: Sending to a Specific Phone

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseInstanceIdService.java
@@ -0,0 +1,23 @@
+package android.example.com.squawker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class SquawkFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String TAG = SquawkFirebaseInstanceIdService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // This method is blank, but if you were to build a server that stores users token
+        // information, this is where you'd send the token to the server.
+    }
+}
